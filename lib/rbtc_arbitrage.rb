62d41cff37ac8a3de94468d12b4fb5841585f6eb
Hank Stoever <hstove@gmail.com>
2013-12-18 08:31:24 -0800

    Adds RbtcArbitrage.clients method

--- lib/rbtc_arbitrage.rb
+++ lib/rbtc_arbitrage.rb
@@ -6,4 +6,9 @@ require 'btce'
 Dir["#{File.dirname(__FILE__)}/rbtc_arbitrage/**/*.rb"].each { |f| require(f) }
 
 module RbtcArbitrage
+  def self.clients
+    RbtcArbitrage::Clients.constants.collect do |c|
+      RbtcArbitrage::Clients.const_get(c)
+    end
+  end
 end
---
+++ spec/rbtc_arbitrage_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe RbtcArbitrage do
+  describe ".clients" do
+    it "includes clients" do
+      clients = RbtcArbitrage.clients
+      clients.should include(RbtcArbitrage::Clients::MtGoxClient)
+      clients.should include(RbtcArbitrage::Clients::BitstampClient)
+    end
+  end
+end
